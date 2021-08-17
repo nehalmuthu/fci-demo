f8dbe3ff8280304eb71daabd99e6d3031c248dcd
nehalmuthu <36196439+nehalmuthu@users.noreply.github.com>
2021-08-17 10:31:17 +0530

    generate bpl data

--- Models.py
+++ Models.py
@@ -12,3 +12,25 @@ def remove_outliers(df, cols):
     for col in cols:
         df = df[np.abs(stats.zscore(df[col]) <= 3)].reset_index(drop=True)
     return df
+
+
+
+def generate_bpl_data(pop, bpl, bpl_cr):
+    states = bpl['State.UT'].unique()
+    states
+    for state in states:
+        perc = bpl[(bpl['State.UT'] == state) & (bpl['year'] == 2011)]['percent'].values[0]
+        for year in range(2012, 2020):
+            perc = perc + bpl_cr
+            new_entry = pd.DataFrame({'State.UT':[state], 'percent':[perc], 'year':[year]})
+            bpl = pd.concat([bpl, new_entry], axis=0)
+
+    state = "ANDHRA PR"
+
+    bpl = bpl[~((bpl['year'] > 2013) & (bpl['State.UT'] == state))]
+
+    bpl = pd.merge(bpl, pop, on=['State.UT', 'year'])
+    bpl['bpl_pop'] = bpl['percent'] * bpl['Population'] / 100
+    bpl = bpl[(bpl['bpl_pop'] > 0)]
+    bpl['log_bpl_pop'] = np.log1p(bpl['Population'])
+    return bpl
